b477f621f1c06f039d80c5bd97fdc80bc46525d6
Leo Jade Rosauro <43976234+scheduler007@users.noreply.github.com>
2018-11-08 15:32:50 +0800

    Add files via upload

---
+++ headfooter.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AA</title>
+</head>
+<style type="text/css">
+	
+	header
+	{
+		border-style: dashed;
+
+
+	}
+</style>
+<body>
+	<header>
+	<a href="Resume.php">Home</a>
+	<a href="">Skills</a>
+	<a href="">Achievements</a>
+	<a href="">Gallery</a>
+	</header>
+</body>
+</html>
